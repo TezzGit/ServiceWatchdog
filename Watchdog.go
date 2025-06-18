a8dbef367aff4f5095d67fdf910929316d788a23
Daniel Terry <daniel.terry@ebetsystems.com.au>
2025-06-18 12:28:26 +1000

    add windows service logic structure

--- Watchdog.go
+++ Watchdog.go
@@ -1,7 +1,74 @@
+// https://dev.to/cosmic_predator/writing-a-windows-service-in-go-1d1m
+
 package Watchdog
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/debug"
+)
+
+const DEBUG_MODE = true
+
+type myService struct{}
+
+func (m *myService) Execute(args []string, r <-chan svc.ChangeRequest, status chan<- svc.Status) (bool, uint32) {
+	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown | svc.AcceptPauseAndContinue
+
+	tick := time.Tick(30 * time.Second)
+	status <- svc.Status{State: svc.StartPending}
+	status <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+
+loop:
+	for {
+		select {
+		case <-tick:
+			log.Print("Tick Handled...!")
+		case c := <-r:
+			switch c.Cmd {
+			case svc.Interrogate:
+				status <- c.CurrentStatus
+			case svc.Stop, svc.Shutdown:
+				log.Print("Shutting service...!")
+				break loop
+			case svc.Pause:
+				status <- svc.Status{State: svc.Paused, Accepts: cmdsAccepted}
+			case svc.Continue:
+				status <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+			default:
+				log.Printf("Unexepcted service control request #%d", c)
+			}
+		}
+	}
+	status <- svc.Status{State: svc.StopPending}
+	return false, 1
+}
+
+func runService(name string, isDebug bool) {
+	if isDebug {
+		err := debug.Run(name, &myService{})
+		if err != nil {
+			log.Fatalln("Error running service in debug mode.")
+		}
+	} else {
+		err := svc.Run(name, &myService{})
+		if err != nil {
+			log.Fatalln("Error running service in Service Control mode.")
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	f, err := os.OpenFile("debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error opening file: %v", err))
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	runService("myservice", DEBUG_MODE)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ServiceWatchdog/ServiceWatchdog
 
 go 1.24.3
+
+require golang.org/x/sys v0.33.0
